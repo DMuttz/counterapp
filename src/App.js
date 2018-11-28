fbd295dec1072f815d9bbb8fc2853e305c9ab830
DMuttz <dmutter1987@gmail.com>
2018-11-27 20:38:22 -0600

    first commit

---
+++ src/App.js
@@ -0,0 +1,17 @@
+import React, { Component } from 'react';
+import './App.css';
+
+class App extends Component {
+  render() {
+    return (
+      <div className="App">
+      
+<button onClick={("Click Me")}>
+  Click Me
+</button>
+      </div>
+    );
+  }
+}
+
+export default App;
